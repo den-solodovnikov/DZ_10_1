1d9f087ea2021d4ac512e2fcbb6f7c625249c968
Den Sol <densfmost@mail.ru>
2026-02-02 10:38:18 +0300

    add function mask_account_card

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from src.masks import get_mask_account
 from src.masks import get_mask_card_number
+from src.widget import mask_account_card
 
 __name__ = "main"
 
 print(get_mask_card_number(7000792289606361))  # 7000 79** **** 6361
 print(get_mask_account(73654108430135874305))  # **4305
+print(mask_account_card('Maestro 1596837868705199'))
---
+++ src/widget.py
@@ -0,0 +1,14 @@
+import re
+from src.masks import get_mask_card_number, get_mask_account
+
+
+def mask_account_card(number_account: str) -> str:
+    """ Функция принимает строку тип и номер карты или счета,
+    возвращает строку с замаскированным номером.
+    """
+    new_number_account = ''.join(re.findall('[0-9]', number_account, re.M))
+    if 'Счет' in number_account:
+        number_account_masked = number_account.replace(new_number_account, "") + (get_mask_account(int(new_number_account)))
+    else:
+        number_account_masked = number_account.replace(new_number_account, "") + get_mask_card_number(int(new_number_account))
+    return number_account_masked
